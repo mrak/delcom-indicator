22f927828e16c5bf96f34ce9a261e29aec9a26f4
Eric Mrak <mail@ericmrak.info>
2014-04-10 10:10:59 -0700

    adding purple light (R + B)

--- DelcomIndicator.js
+++ DelcomIndicator.js
@@ -1,105 +1,103 @@
 var hid = require('node-hid');
 
 function DelcomIndicator(){
-    var vendorId = 0xFC5;
-    var productId = 0xB080;
+  var vendorId = 0xFC5;
+  var productId = 0xB080;
 
-    this.findDevice = function(){
-        var devices = hid.devices(vendorId, productId);
-        if (devices !== undefined){
-            return devices[0];
-        }
-    };
-
-    this.green = 0xFE;
-    this.red = 0xFD;
-    this.blue= 0xFB;
-    this.off = 0xFF;
-
-    this.solid = 2;
-    this.write = 101;
-    this.flash = 20;
-
-    this.device = this.findDevice();
-    if (this.device){
-        this.deviceConnection = new hid.HID(this.device.path);
+  this.findDevice = function(){
+    var devices = hid.devices(vendorId, productId);
+    if (devices !== undefined){
+      return devices[0];
     }
+  };
+
+  this.green = 0xFE;
+  this.red = 0xFD;
+  this.blue = 0xFB;
+  this.purple = 0xF9;
+  this.off = 0xFF;
+
+  this.solid = 2;
+  this.write = 101;
+  this.flash = 20;
+
+  this.device = this.findDevice();
+  if (this.device){
+    this.deviceConnection = new hid.HID(this.device.path);
+  }
 };
 
+//DelcomIndicator.prototype.open = function() {
+  //this.device = this.findDevice();
+  //if (this.device){
+    //this.deviceConnection = new hid.HID(this.device.path);
+  //}
+//};
 
 DelcomIndicator.prototype.isConnected = function(){
-    return this.device !== undefined;
+  return this.device !== undefined;
 };
 
 DelcomIndicator.prototype.isOpen = function() {
-    return this.deviceConnection;
+  return this.deviceConnection;
 };
 
 DelcomIndicator.prototype.close = function(){
-    if (this.deviceConnection){
-        this.deviceConnection.close();
-        this.deviceConnection = undefined;
-    }
+  if (this.deviceConnection){
+    this.deviceConnection.close();
+    this.deviceConnection = undefined;
+  }
+};
+
+DelcomIndicator.prototype.solidColor = function(color) {
+  if (!this.isOpen()) { throw "Device is not open"; }
+
+  this.deviceConnection.write([this.write, this.solid, color]);
 };
 
 DelcomIndicator.prototype.solidGreen = function(){
-    if (this.isOpen()){
-        this.deviceConnection.write([this.write, this.solid, this.green]);
-    } else {
-        throw "Device is not open";
-    }
+  this.solidColor(this.green);
 };
 
 DelcomIndicator.prototype.solidRed = function(){
-    if (this.isOpen()){
-        this.deviceConnection.write([this.write, this.solid, this.red]);
-    } else {
-        throw "Device is not open";
-    }
+  this.solidColor(this.red);
 };
 
 DelcomIndicator.prototype.solidBlue = function(){
-    if (this.isOpen()){
-        this.deviceConnection.write([this.write, this.solid, this.blue]);
-    } else {
-        throw "Device is not open";
-    }
+  this.solidColor(this.blue);
 };
 
+DelcomIndicator.prototype.solidPurple = function(){
+  this.solidColor(this.purple);
+};
+
+DelcomIndicator.prototype.flashColor = function(color, bit){
+  if (!this.isOpen()){ throw "Device is not open"; }
+
+  this.deviceConnection.write([this.write, this.solid, color]);
+  this.deviceConnection.write([this.write, this.flash, 0, bit]);
+};
 DelcomIndicator.prototype.flashGreen = function(){
-    if (this.isOpen()){
-        this.deviceConnection.write([this.write, this.solid, this.green]);
-        this.deviceConnection.write([this.write, this.flash, 0, 1]);
-    } else {
-        throw "Device is not open";
-    }
+  this.flashColor(this.green, 1)
 };
 
 DelcomIndicator.prototype.flashRed = function(){
-    if (this.isOpen()){
-        this.deviceConnection.write([this.write, this.flash, this.red]);
-        this.deviceConnection.write([this.write, this.flash, 0, 2]);
-    } else {
-        throw "Device is not open";
-    }
+  this.flashColor(this.red, 2)
 };
 
 DelcomIndicator.prototype.flashBlue = function(){
-    if (this.isOpen()){
-        this.deviceConnection.write([this.write, this.flash, this.blue]);
-        this.deviceConnection.write([this.write, this.flash, 0, 4]);
-    } else {
-        throw "Device is not open";
-    }
+  this.flashColor(this.blue, 4)
+};
+
+DelcomIndicator.prototype.flashPurple = function(){
+  this.flashColor(this.purple, 6)
 };
 
 DelcomIndicator.prototype.turnOff = function(){
-    if (this.isOpen()){
-        this.deviceConnection.write([this.write, this.solid, this.off]);
-        this.deviceConnection.write([this.write, this.flash, this.off]);
-    } else {
-        throw "Device is not open";
-    }
+  if (!this.isOpen()){ throw "Device is not open"; }
+
+  this.deviceConnection.write([this.write, this.solid, this.off]);
+  this.deviceConnection.write([this.write, this.flash, this.off]);
 };
 
 module.exports = DelcomIndicator;
